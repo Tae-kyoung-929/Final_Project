2cc22b665bdd62edb44e18dbd94e1ade642a0d76
Tae-kyoung-929 <thonkee929@gmail.com>
2024-08-15 11:40:50 -0700

    initial commit

---
+++ imagenet.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+
+import sys
+import argparse
+
+from jetson_inference import imageNet
+from jetson_utils import videoSource, videoOutput, cudaFont, Log
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Classify a live camera stream using an image recognition DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, 
+                                 epilog=imageNet.Usage() + videoSource.Usage() + videoOutput.Usage() + Log.Usage())
+
+parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="googlenet", help="pre-trained model to load (see below for options)")
+parser.add_argument("--topK", type=int, default=1, help="show the topK number of class predictions (default: 1)")
+
+try:
+	args = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+
+# load the recognition network
+net = imageNet(args.network, sys.argv)
+
+# note: to hard-code the paths to load a model, the following API can be used:
+#
+# net = imageNet(model="model/resnet18.onnx", labels="model/labels.txt", 
+#                 input_blob="input_0", output_blob="output_0")
+
+# create video sources & outputs
+input = videoSource(args.input, argv=sys.argv)
+output = videoOutput(args.output, argv=sys.argv)
+font = cudaFont()
+
+# process frames until EOS or the user exits
+while True:
+    # capture the next image
+    img = input.Capture()
+
+    if img is None: # timeout
+        continue  
+
+    # classify the image and get the topK predictions
+    # if you only want the top class, you can simply run:
+    #   class_id, confidence = net.Classify(img)
+    predictions = net.Classify(img, topK=args.topK)
+
+    # draw predicted class labels
+    for n, (classID, confidence) in enumerate(predictions):
+        classLabel = net.GetClassLabel(classID)
+        confidence *= 100.0
+        
+
+        print(f"imagenet:  {confidence:05.2f}% class #{classID} ({classLabel})")
+
+        if classLabel == "rubber eraser, rubber, pencil eraser":
+            print("This is a pencil. This can be used for writing notes in class or drawing sketches.\n The tip of a pencil can be pointy, so be careful not to poke someone with it!")
+        elif classLabel == "ballpoint, ballpoint pen, ballpen, Biro":
+            print("This is a pen. It can be used to take notes and you can use different colors to emphasize important things!")
+        elif classLabel == "pencil box, pencil case":
+            print("This is a pencil case. You can store different kinds of writing utensils so you don't lose them!")
+        elif classLabel == "pencil sharpener":
+            print("This is a pencil sharpener. By sticking a blunt pencil in it and rotating it, you can sharpen your pencil so that it is sharp again!")
+        elif classLabel == "rule, ruler":
+            print("This is a ruler. You can use it to measure a specific distance or draw a straight line with a pencil!")
+        else:
+            print("Unable to identify the object. Please try again.")
+
+        font.OverlayText(img, text=f"{confidence:05.2f}% {classLabel}", 
+                         x=5, y=5 + n * (font.GetSize() + 5),
+                         color=font.White, background=font.Gray40)
+                         
+    # render the image
+    output.Render(img)
+
+    # update the title bar
+    output.SetStatus("{:s} | Network {:.0f} FPS".format(net.GetNetworkName(), net.GetNetworkFPS()))
+
+    # print out performance info
+    net.PrintProfilerTimes()
+
+    # exit on input/output EOS
+    if not input.IsStreaming() or not output.IsStreaming():
+        break
